4afa99afcc02e99515209fbd94b5c1eedf5656a4
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-08-17 05:46:41 +0100

    added exit functionality

--- script.js
+++ script.js
@@ -42,3 +42,30 @@ function exit() {
 	}
 }
 
+function atm() {
+    var name = prompt("Enter your name") ; 
+    var capitalized = name.toUpperCase();
+    var bank = prompt("Enter Bank Name");
+    // var bankName = Number(prompt("What is your bank name?\n 1. GTB\n2.Access\n3.Stanbic-IBTC\n4.Fidelity\n5.Sterling\n6.Skye\n7.Firstbank\n8.Others"));
+    //prompt user for choice.
+	
+    var selectAccount = Number(prompt("Which type of account do you have?\n 1. Savings\n 2. Current\n 3. Credit"));
+    // input pin
+    // check pin
+    // send option
+    if (selectAccount < 4){
+        var choice = parseInt(prompt(`Welcome!!! ${capitalized} to ${bank}\n What would you like to do?\n 1.) Check Balance\n 2.) Deposit\n 3.) Withdrawal\n 4.) Exit`)); 
+    }if (choice === 1) {
+		get_balance();
+	} else if (choice === 2) {
+		make_deposit();
+	} else if (choice === 3) {
+		make_withdrawal();
+	} else if (choice === 4) {
+		exit();
+	} else {
+		error();
+	}
+}
+
+atm();
